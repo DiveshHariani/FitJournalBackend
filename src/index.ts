e9f0375ffd8cabfbdc9df284503070edff8715f6
DiveshHariani <56154666+DiveshHariani@users.noreply.github.com>
2023-07-05 16:52:22 -0700

    Added route

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ app.use('/', (req: Request, res: Response) => {
     res.send('Hello World');
 });
 
+app.use('/app', (req: Request, res: Response) => {
+    res.send("Hello Cloud App");
+})
+
 app.listen(3000, () => {
     console.log("Server is running");
 })
