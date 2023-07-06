8c85501b7bbdce41539cba715b327708268bc496
DiveshHariani <56154666+DiveshHariani@users.noreply.github.com>
2023-07-05 17:18:51 -0700

    Route

--- build/index.js
+++ build/index.js
@@ -5,11 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.use('/', (req, res) => {
-    res.send('Hello World');
-});
 app.use('/app', (req, res) => {
     res.send("Hello Cloud App");
+});
+app.use('/', (req, res) => {
+    res.send('Hello World');
 });
 app.listen(3000, () => {
     console.log("Server is running");
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
+app.use('/app', (req: Request, res: Response) => {
+    res.send("Hello Cloud App");
+});
+
 app.use('/', (req: Request, res: Response) => {
     res.send('Hello World');
 });
 
-app.use('/app', (req: Request, res: Response) => {
-    res.send("Hello Cloud App");
-})
-
 app.listen(3000, () => {
     console.log("Server is running");
-})
+});
